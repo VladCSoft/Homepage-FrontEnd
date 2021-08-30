aec074076b44ac5d5c22e4a40881e5672f399642
VladCSoft <vlad.cojocaru98@gmail.com>
2021-08-30 16:09:57 +0300

    Improved scalablity for medium devices

--- src/App.js
+++ src/App.js
@@ -27,12 +27,14 @@ function App() {
           <button className="WTButton"> Inscrie-te la cursuri! </button>
           </div>
       </div>
-     {/*     <p className="Acreditation-Text" > Acreditarile GSP Training Center </p>
       <div className="Acreditation-Block">
+            <p className="Acreditation-Text" > Acreditarile GSP Training Center </p>
+            <div className="Certification-Block"> 
             <div className="Certificate1"> </div>
             <div className="Certificate2"> </div>
             <div className="Certificate3"> </div>
             <div className="Certificate4"> </div>
+            </div>
       </div> 
       <div className="Drilling-Simulator-Block"> 
             <h3 className="DSTHeader1"> Simulatorul nostru  </h3>
@@ -42,101 +44,126 @@ function App() {
       </div>
       <div className="Rig-Image"> </div>
       <div className="Curved-Line"> </div>
-      <div className="Information-Background"> </div>
-      <div className="Platform-Image"> </div>
-      <div className="Training-Image">  </div>
-      <div className="Career-Oil-Gas-Block">
-            <h3 className="CIGHeader1"> GSP Training Center </h3>
-            <h1 className="CIGHeader2"> Construieste-ti cariera in domeniul <span className="CIGHeader3-Blue"> Oil & Gas </span> </h1>
-            <h3 className="CIGHeader3"> Descopera cursurile GSP Training Center si acrediteaza-te pentru domeniul Oil & Gas.</h3>
+      <div className="Information-Background">
+            <div className="Platform-Image"> </div>
+            <div className="Training-Image">  </div>
+            <div className="Career-Oil-Gas-Block">
+                  <h3 className="CIGHeader1"> GSP Training Center </h3>
+                  <h1 className="CIGHeader2"> Construieste-ti</h1>
+                  <h1 className="CIGHeader2-2"> cariera in domeniul  </h1>
+                  <h1 className="CIGHeader2-3"> <span className="CIGHeader3-Blue"> Oil & Gas </span> </h1>
+                  <h3 className="CIGHeader3"> Descopera cursurile GSP Training Center si</h3>
+                  <h3 className="CIGHeader3-2"> acrediteaza-te pentru domeniul Oil & Gas. </h3>
+            </div> 
             <button className="CIGButton"> AFLA MAI MULTE </button>
-      </div>
-      <div className="Online-Training-Block">
-            <h3 className="OTHeader1"> PLATFORMA DE EDUCATIE ONLINE</h3> 
-            <h1 className="OTHeader2"> GSP Online Training </h1>
-            <h3 className="OTHeader3"> Eficientizam procesele de invatare din domeniul Oil & Gas. Descopera platforma online. </h3>
-            <button className="OTButton"> AFLA MAI MULTE </button>
+            <div className="Online-Training-Block">
+                  <h3 className="OTHeader1"> PLATFORMA DE EDUCATIE ONLINE</h3> 
+                  <h1 className="OTHeader2"> GSP Online Training </h1>
+                  <h3 className="OTHeader3"> Eficientizam procesele de invatare din domeniul Oil & Gas. Descopera platforma online. </h3>
+                  <h3 className="OTButton"> AFLA MAI MULTE </h3>
+            </div>
       </div>
       <div className="Globe-Image"> </div>
-      <div className="Extra-Background"> </div>
+      <div className="Extra-Background"> 
       <div className="Career-Block">
             <h1 className="CHeader1"> Ofera <span className="CHeader1-Red"> noi perspective </span> carierei tale  </h1>
             <h3 className="CHeader2"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Augue tellus urna,mi velit diam. Turpis diam amet massa id. </h3>
             <button className="CButton"> AFLA MAI MULTE </button>
       </div>
+      </div>
       <div className="CommentBlock1">
-            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Augue tellus urna,mi velit diam. </p>
+            <p className="Lorem1"> Lorem ipsum dolor sit </p>
+            <p className="Lorem2"> amet, consectetur  </p>
+            <p className="Lorem3"> adipiscing elit. Augue </p>
+            <p className="Lorem4"> tellus urna,mi velit diam. </p>
             <div className="ProfilePicture1"></div>
             <h4 className="CommentName"> Alexandru Andreescu </h4>
             <h5 className="CommentJob"> Project Manager </h5>
       </div>
       <div className="CommentBlock2">
-            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Augue tellus urna,mi velit diam. </p>
+            <p className="Lorem1"> Lorem ipsum dolor sit </p>
+            <p className="Lorem2"> amet, consectetur  </p>
+            <p className="Lorem3"> adipiscing elit. Augue </p>
+            <p className="Lorem4"> tellus urna,mi velit diam. </p>
             <div className="ProfilePicture2"></div>
             <h3 className="CommentName" > Irina Tofan </h3>
             <h4 className="CommentJob"> Chief Drilling Operator </h4>
       </div>
       <div className="CommentBlock3">
-            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Augue tellus urna,mi velit diam. </p>
+            <p className="Lorem1"> Lorem ipsum dolor sit </p>
+            <p className="Lorem2"> amet, consectetur  </p>
+            <p className="Lorem3"> adipiscing elit. Augue </p>
+            <p className="Lorem4"> tellus urna,mi velit diam. </p>
             <div className="ProfilePicture3"> </div>
             <h3 className="CommentName"> Maria Dragomiroiu</h3>
             <h4 className="CommentJob"> Search & Rescue</h4>
       </div>
       <div className="CommentBlock4">
-            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Augue tellus urna,mi velit diam. </p>
+            <p className="Lorem1"> Lorem ipsum dolor sit </p>
+            <p className="Lorem2"> amet, consectetur  </p>
+            <p className="Lorem3"> adipiscing elit. Augue </p>
+            <p className="Lorem4"> tellus urna,mi velit diam. </p>
             <div className="ProfilePicture4"> </div>
             <h3 className="CommentName"> Bogdan Stoian </h3>
             <h4 className="CommentJob"> Capitan Via Explorere</h4> 
       </div>
       <p className="Capture-Interest-Title"> As good as it sounds </p>
-      <div className="SpecialBlock"> 
+      <div className="SpecialBlock">
       <div className="Audio-Block"> 
             <div className="Audio-Image"> </div>
             <h2 className="FeaturesHeader1"> Audio Books </h2>
-            <h3 className="FeaturesHeader2"> Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h3>
+            <h3 className="FeaturesHeader2"> Lorem ipsum dolor sit amet, </h3>
+            <h3 className="FeaturesHeader3"> consectetur adipiscing elit.</h3>
       </div>
       <div className="Confidential-Block">
             <div className="Confidential-Image"> </div>
             <h2 className="FeaturesHeader1"> 100% Confidential </h2>
-            <h3 className="FeaturesHeader2"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. </h3>
+            <h3 className="FeaturesHeader2"> Lorem ipsum dolor sit amet, </h3>
+            <h3 className="FeaturesHeader3"> consectetur adipiscing elit.</h3>
       </div>
       <div className="Certification-Block">
             <div className="Certification-Image"> </div>
             <h2 className="FeaturesHeader1"> Certificari Internationale </h2>
-            <h3 className="FeaturesHeader2"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. </h3>
+            <h3 className="FeaturesHeader2"> Lorem ipsum dolor sit amet, </h3>
+            <h3 className="FeaturesHeader3"> consectetur adipiscing elit. </h3>
       </div>
       <div className="Interactive-Training-Block">
             <div className="Interactive-Training-Image"> </div>
             <h2 className="FeaturesHeader1"> Training Interactiv </h2>
-            <h3 className="FeaturesHeader2"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. </h3>
+            <h3 className="FeaturesHeader2"> Lorem ipsum dolor sit amet, </h3>
+            <h3 className="FeaturesHeader3"> consectetur adipiscing elit.</h3>
       </div>
       </div>
       <div className="Info-Banner">
             <p className="BannerHeader"> Cel mai modern centru de training Oil & Gas din Romania</p>
             <p className="BannerButton"> AFLA MAI MULTE  </p>
       </div>
+      <div className="Footer-Special-Block"> 
+      <div className="GSP-Trademark"> 
       <div className="GSP-Training-Logo"> </div>
       <p className="Trademark-Text"> 2021 GSP Training.All Rights Reserved </p>
+      </div>
+      <div className="Bottom-Nav">
       <div className="Bottom-Bar-Items1"> 
             <ul className="Bottom-Navigation-Bar-Items1">
-              <li> HOME </li>
-              <li> DESPRE NOI </li>
-              <li> CURSURI </li>
-              <li> SOCIAL MEDIA </li>
+              <li className="Item-Bottom"> HOME </li>
+              <li className="Item-Bottom"> DESPRE NOI </li>
+              <li className="Item-Bottom"> CURSURI </li>
+              <li className="Item-Bottom"> SOCIAL MEDIA </li>
             </ul>
       </div>
       <div className="Bottom-Bar-Items2">
             <ul className="Bottom-Navigation-Bar-Items2">
-              <li className="PlatformaT"> PLATFORMA </li>
-              <li className="StiriT"> STIRI </li>
-              <li className="ContactT"> CONTACT</li>
+              <li> PLATFORMA </li>
+              <li className="Item-Stiri"> STIRI </li>
+              <li className="Item-Contact"> CONTACT</li>
+              <li className="Facebook-Logo"> </li>
+              <li className="Twitter-Logo"> </li>
+              <li className="Instagram-Logo"> </li>
+              <li className="Youtube-Logo"> </li> 
             </ul>
       </div>
-      <div className="Logo-Block"> 
-              <div className="Facebook-Logo"> </div>
-              <div className="Twitter-Logo"> </div>
-              <div className="Instagram-Logo"> </div>
-              <div className="Youtube-Logo"></div>
+      </div>
       </div>
       <hr className="Horizontal-Bar" /> 
       <div className="Footer">
@@ -147,7 +174,7 @@ function App() {
                 <li> Contact </li>
                 <li> Help </li>
               </ul>
-  </div> */}
+  </div> 
       </body> 
     </div>
   );
